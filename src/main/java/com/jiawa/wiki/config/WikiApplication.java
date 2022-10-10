8aa8e95bead400f94f500732d666de68d3d16a0e
leo <1946840187@qq.com>
2022-10-10 19:33:00 +0800

    开发了Hello接口

--- src/main/java/com/jiawa/wiki/config/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/config/WikiApplication.java
@@ -1,4 +1,4 @@
-package com.jiawa.wiki;
+package com.jiawa.wiki.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+@ComponentScan("com.jiawa.wiki")
 
 @RestController
 public class TestController {
