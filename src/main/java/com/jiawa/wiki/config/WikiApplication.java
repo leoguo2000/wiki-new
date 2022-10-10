6c72766e9b7eb61e515773e2d55883153df2e473
leo <1946840187@qq.com>
2022-10-10 19:42:16 +0800

    开发了http测试接口，可以离开浏览器进行单独操作

--- src/main/java/com/jiawa/wiki/config/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/config/WikiApplication.java
@@ -2,7 +2,10 @@ package com.jiawa.wiki.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.jiawa.wiki")
+/*componentscan注释代表启动类可以搜寻到包的文件*/
 @SpringBootApplication
 public class WikiApplication {
 
--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -3,11 +3,10 @@ package com.jiawa.wiki.controller;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@ComponentScan("com.jiawa.wiki")
 
 @RestController
 public class TestController {
-    @RequestMapping("/Hello")
+    @RequestMapping("/Hello")/*浏览器默认使用get请求 如果更改访问方法（post） 会有405错误*/
     public String Hello(){
         return "Hello World!";
     }
